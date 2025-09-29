679d7a69b03bfa6ebe4a7c029426d582609977e2
Yusrah <mohammedyusi6@gmail.com>
2025-09-29 13:57:31 +0100

    trying again

--- frontend/src/components/UnifiedMessage.tsx
+++ frontend/src/components/UnifiedMessage.tsx
@@ -43,7 +43,7 @@ export const UnifiedMessage: React.FC<Props> = ({ message, isLatest }) => {
     };
 
     return (
-      <div className={`text-text-primary leading-relaxed ${isUser ? 'text-accent/90' : ''}`}>
+      <div className={`text-text-primary leading-relaxed ${isUser ? 'text-accent' : ''}`}>
         <ReactMarkdown
           remarkPlugins={[remarkGfm]}
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
